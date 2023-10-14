95f6bc4efd88ebafaacf9a46a19c900edb415a92
x3imal <aasurov@gmail.com>
2023-10-14 16:03:59 +0300

    created project

---
+++ src/main/java/pro/sky/animalsheltertelegrambot/AnimalShelterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.animalsheltertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterTelegramBotApplication.class, args);
+    }
+
+}
