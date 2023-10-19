53f2e20f1ea332bd1bcd550f12c188a0379e4222
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-19 14:54:40 +0300

    19/chore: TODO style

--- src/main/java/pro/sky/animalsheltertelegrambot/animal_status/Status.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/animal_status/Status.java
@@ -3,7 +3,7 @@ package pro.sky.animalsheltertelegrambot.animal_status;
 /**
  * Перечисление возможных статусов у животного
  * <p>
- * TODO установить статусы
+ * TODO: установить статусы
  */
 public enum Status {
 
