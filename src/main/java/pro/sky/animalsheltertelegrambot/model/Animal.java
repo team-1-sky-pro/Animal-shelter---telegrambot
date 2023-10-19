8d4ff0faa996bb411a5c1a94f60a8b293ada9032
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-19 14:53:41 +0300

    19/feat: add Animal entity

---
+++ src/main/java/pro/sky/animalsheltertelegrambot/model/Animal.java
@@ -0,0 +1,50 @@
+package pro.sky.animalsheltertelegrambot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import pro.sky.animalsheltertelegrambot.animal_status.Status;
+
+import java.util.Date;
+
+/**
+ * Animal сущность
+ * @author Rnd-mi
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Animal {
+
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String name;
+
+    private Date birthDate;
+
+    /**
+     * Описание животного, например: внешние особенности, наличие заболеваний
+     */
+    private String description;
+
+    /**
+     * Внешний ключ: ID приюта из таблицы 'shelters', к которому относится данное животное
+     * @see Shelter
+     */
+    private Long shelterId;
+
+    /**
+     * Внешний ключ: ID фото из таблицы 'photos'
+     * @see Photo
+     */
+    private Long photoID;
+
+    /**
+     * Статус, который присвоен животному
+     * @see Status
+     */
+    private Status status;
+}
