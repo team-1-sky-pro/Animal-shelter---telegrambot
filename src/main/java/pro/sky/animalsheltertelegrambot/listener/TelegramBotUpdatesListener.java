c65e29c5b001268f9c16537b94724623f4eb95bf
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-19 11:11:43 +0300

    23/feat: add updates listener class

---
+++ src/main/java/pro/sky/animalsheltertelegrambot/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,37 @@
+package pro.sky.animalsheltertelegrambot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Service class that listens to updates from telegram bot
+ */
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener{
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+}
