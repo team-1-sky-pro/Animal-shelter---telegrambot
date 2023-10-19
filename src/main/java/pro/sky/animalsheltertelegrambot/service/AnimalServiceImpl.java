813e4b8dc140689541986a9755a97044fd1af8b2
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-19 15:36:34 +0300

    19/feat: add resource 'animals' and CRUD endpoints for it

---
+++ src/main/java/pro/sky/animalsheltertelegrambot/service/AnimalServiceImpl.java
@@ -0,0 +1,28 @@
+package pro.sky.animalsheltertelegrambot.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.animalsheltertelegrambot.model.Animal;
+
+@Service
+public class AnimalServiceImpl implements AnimalService {
+
+    @Override
+    public void addAnimal(Animal animal) {
+
+    }
+
+    @Override
+    public void updateAnimal(Long id, Animal animal) {
+
+    }
+
+    @Override
+    public Animal getAnimal(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteAnimal(Long id) {
+
+    }
+}
