c173f07ff4805d1a429a17c79b0b6149714dd3d3
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-19 11:11:21 +0300

    23/feat: add configuration for telegram bot

---
+++ src/main/java/pro/sky/animalsheltertelegrambot/configuration/TelegramBotConfiguration.java
@@ -0,0 +1,31 @@
+package pro.sky.animalsheltertelegrambot.configuration;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.DeleteMyCommands;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Class which responsible for configuring telegram bot
+ */
+@Configuration
+public class TelegramBotConfiguration {
+
+    /**
+     * Retrieving token value from application.properties
+     */
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    /**
+     * Generating TelegramBot bean
+     * @return TelegramBot instance
+     */
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
+}
