9113211a0c79c906dc378f604db268303ea835ea
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-20 18:37:20 +0300

    22/chore: add @Service on ReportServiceImpl

--- src/main/java/pro/sky/animalsheltertelegrambot/service/ReportServiceImpl.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/service/ReportServiceImpl.java
@@ -1,7 +1,9 @@
 package pro.sky.animalsheltertelegrambot.service;
 
+import org.springframework.stereotype.Service;
 import pro.sky.animalsheltertelegrambot.model.Report;
 
+@Service
 public class ReportServiceImpl implements ReportService {
     @Override
     public void addReport(Report report) {
