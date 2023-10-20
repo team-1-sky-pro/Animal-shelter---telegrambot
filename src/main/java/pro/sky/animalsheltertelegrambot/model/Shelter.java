45d553cb032f8a1f6bce6cdf5746e543a1347a8b
SyutinS <syut@mail.ru>
2023-10-20 22:13:39 +0300

    исправил имена

--- src/main/java/pro/sky/animalsheltertelegrambot/model/Shelter.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/model/Shelter.java
@@ -23,11 +23,10 @@ public class Shelter {
     @Id
     @GeneratedValue
     private Long id;
-    private String address;
-    private String type;
-    private String security_contact;
-    private String working_hours;
-    private String safety_guide;
+    private String contact;
+    private String shelterType;
+    private String securityContact;
+    private String workingHours;
 
     /**
      * животные живущие в определенном приюте
