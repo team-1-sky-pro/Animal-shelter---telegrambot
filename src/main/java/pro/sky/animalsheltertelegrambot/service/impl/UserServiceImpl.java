74f37307cc1c8e03fc115cfd71e18def44ec5ba4
Anna <anna051977@gmail.com>
2023-10-21 01:00:14 +0300

    in Repositories classes added constructors

--- src/main/java/pro/sky/animalsheltertelegrambot/service/impl/UserServiceImpl.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/service/impl/UserServiceImpl.java
@@ -25,6 +25,10 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User addUser(User user) {
         return userRepository.save(user);
