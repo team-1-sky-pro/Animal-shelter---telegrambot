c9f3165b0f55c6710d44d22fa2fa2043d0595bc4
x3imal <aasurov@gmail.com>
2023-10-20 19:14:30 +0300

    21: add swagger-doc

--- src/main/java/pro/sky/animalsheltertelegrambot/controller/PhotoController.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/controller/PhotoController.java
@@ -1,5 +1,7 @@
 package pro.sky.animalsheltertelegrambot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,20 +13,30 @@ import pro.sky.animalsheltertelegrambot.service.PhotoService;
 import pro.sky.animalsheltertelegrambot.utils.ErrorUtils;
 
 @RestController
-@RequestMapping("/photo")
+@RequestMapping("/photos")
 @RequiredArgsConstructor
 public class PhotoController {
 
 
     private final PhotoService photoService;
 
-    /**
-     * Добавляет новое фото в систему.
-     *
-     * @param photo Данные фотографии для добавления.
-     * @param result BindingResult, содержащий результаты валидации.
-     * @return ResponseEntity с добавленным фото или списком ошибок.
-     */
+    @Operation(
+            summary = "Добавление новой фотографии в базу",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody (
+                    description = "Новая фотография"
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Успешное добавление фотографии"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Неправильное, неполное заполнение полей сущности"
+                    )
+            },
+            tags = "Photos"
+    )
     @PostMapping
     public ResponseEntity<?> addPhoto(@Valid @RequestBody Photo photo, BindingResult result) {
         if (result.hasErrors()) {
@@ -34,12 +46,20 @@ public class PhotoController {
         return new ResponseEntity<>(newPhoto, HttpStatus.CREATED);
     }
 
-    /**
-     * Получает фото по его ID.
-     *
-     * @param id ID фотографии для получения.
-     * @return ResponseEntity с фотографией или сообщением об ошибке.
-     */
+    @Operation(
+            summary = "Получение фотографии по ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Фотография найдена"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Фотография не найдена"
+                    )
+            },
+            tags = "Photos"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<?> getPhoto(@PathVariable Long id) {
         Photo existPhoto = photoService.getPhoto(id);
@@ -49,14 +69,23 @@ public class PhotoController {
         return new ResponseEntity<>(existPhoto, HttpStatus.OK);
     }
 
-    /**
-     * Обновляет данные фото по его ID.
-     *
-     * @param id ID фотографии для обновления.
-     * @param photo Обновленные данные фотографии.
-     * @param result BindingResult, содержащий результаты валидации.
-     * @return ResponseEntity с обновленным фото или списком ошибок.
-     */
+    @Operation(
+            summary = "Обновление данных фотографии по ID",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody (
+                    description = "Обновленные данные фотографии"
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Фотография успешно обновлена"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Неправильное, неполное заполнение полей сущности или фотография не найдена"
+                    )
+            },
+            tags = "Photos"
+    )
     @PutMapping("/{id}")
     public ResponseEntity<?> updatePhoto(@Valid @PathVariable Long id, @RequestBody Photo photo,
                                             BindingResult result) {
@@ -67,12 +96,20 @@ public class PhotoController {
         return new ResponseEntity<>(updatePhoto, HttpStatus.OK);
     }
 
-    /**
-     * Удаляет фото по его ID.
-     *
-     * @param id ID фотографии для удаления.
-     * @return ResponseEntity с подтверждающим сообщением.
-     */
+@Operation(
+        summary = "Удаление фотографии по ID",
+        responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Фотография успешно удалена"
+                ),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Фотография не найдена"
+                )
+        },
+        tags = "Photos"
+)
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePhoto(@PathVariable Long id) {
         photoService.deletePhoto(id);
