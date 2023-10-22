c7061152fa692f1373b42e30d05e48bfe16b76f5
x3imal <aasurov@gmail.com>
2023-10-22 17:36:01 +0300

    29: добавил исключение AdoptionNotFoundExceptions и добавил в AdoptionServiceImpl документацию JavaDoc

--- src/main/java/pro/sky/animalsheltertelegrambot/service/impl/AdoptionServiceImpl.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/service/impl/AdoptionServiceImpl.java
@@ -1,33 +1,107 @@
 package pro.sky.animalsheltertelegrambot.service.impl;
 
+import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pro.sky.animalsheltertelegrambot.exception.AdoptionNotFoundExceptions;
 import pro.sky.animalsheltertelegrambot.model.Adoption;
+import pro.sky.animalsheltertelegrambot.repository.AdoptionRepository;
 import pro.sky.animalsheltertelegrambot.service.AdoptionService;
 
-
+/**
+ * Реализация сервиса по управлению усыновлением.
+ */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AdoptionServiceImpl implements AdoptionService {
 
+    private final AdoptionRepository adoptionRepository;
 
+    /**
+     * Добавляет новое усыновление.
+     *
+     * @param adoption Усыновление, которую нужно добавить.
+     * @return Добавлено усыновления.
+     */
     @Override
     public Adoption addAdoption(Adoption adoption) {
-        return null;
+        if (adoption == null) {
+            log.error("Attempt to add a null adoption");
+            return null;
+        }
+
+        log.info("Adding new adoption: {}", adoption);
+        return adoptionRepository.save(adoption);
     }
 
+    /**
+     * Получает усыновление по его Id.
+     *
+     * @param id Id усыновления, который требуется получить.
+     * @return Найденное усыновление.
+     * @throws AdoptionNotFoundException Если усыновление не обнаружено.
+     * @throws NullPointerException Если Id равен null.
+     */
     @Override
     public Adoption getAdoption(Long id) {
-        return null;
+        if (id == null) {
+            log.error("Attempt to get adoption with null ID");
+            throw new IllegalArgumentException("ID cannot be null");
+        }
+        log.info("Fetching adoption with ID: {}", id);
+        return adoptionRepository.findById(id).orElseThrow(
+                ()-> new AdoptionNotFoundExceptions("Adoption this Id not found."));
     }
 
+    /**
+     * Обновляет существующее усыновление.
+     *
+     * @param id Id усыновления, который необходимо обновить.
+     * @param adoption Усыновления с обновленными полями.
+     * @return Обновленное усыновление.
+     * @throws NullPointerException Если данные Id или усыновления равны null.
+     */
     @Override
     public Adoption updateAdoption(Long id, Adoption adoption) {
-        return null;
+        if (id == null || adoption == null) {
+            log.error("Invalid ID or adoption data for update");
+            throw new IllegalArgumentException("ID and Adoption cannot be null");
+        }
+        Adoption updatesAdoption = getAdoption(id);
+        if (updatesAdoption == null) {
+            log.error("Adoption with ID {} not found", id);
+            throw new IllegalArgumentException("Updates Adoption and Adoption cannot be null");
+        }
+        updatesAdoption.setAdoptionDate(adoption.getAdoptionDate());
+        updatesAdoption.setTrialEndDate(adoption.getTrialEndDate());
+        updatesAdoption.setActive(adoption.isActive());
+
+        log.info("Updating adoption with ID: {}", id);
+        return adoptionRepository.save(updatesAdoption);
     }
 
+    /**
+     * Удаляет усыновление по его Id.
+     *
+     * @param id Id усыновления, которое необходимо удалить.
+     * @throws EntityExistsException Если усыновление не обнаружено.
+     * @throws NullPointerException Если Id равен null.
+     */
     @Override
     public void deleteAdoption(Long id) {
+        if (id == null) {
+            log.error("Attempt to delete adoption with null ID");
+            throw new IllegalArgumentException("ID cannot be null");
+        }
 
+        if (!adoptionRepository.existsById(id)) {
+            log.error("Adoption with ID {} not found", id);
+            throw new EntityExistsException();
+        }
+
+        log.info("Deleting adoption with ID: {}", id);
+        adoptionRepository.deleteById(id);
     }
 }
