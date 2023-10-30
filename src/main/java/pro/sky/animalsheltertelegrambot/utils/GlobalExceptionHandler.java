e67d6adcd49394294a8a0af4e467b2541c624551
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-30 11:36:39 +0300

    31/feat: добавление GlobalExceptionHandler для обработки исключения при превышении максимально допустимого размера файла

---
+++ src/main/java/pro/sky/animalsheltertelegrambot/utils/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package pro.sky.animalsheltertelegrambot.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+/**
+ * Класс, который отлавливает прописанные в нем типы исключений и выполняет логику по их обработке.
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.warn("MaxUploadSizeExceededException occurred");
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
