e4fb86caabee0b036a31b046f68170eadfd56de2
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-30 17:26:35 +0300

    31/feat: доработка контроллера под методы сервиса

--- src/main/java/pro/sky/animalsheltertelegrambot/controller/PhotoController.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/controller/PhotoController.java
@@ -1,6 +1,7 @@
 package pro.sky.animalsheltertelegrambot.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,7 +17,7 @@ import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Фотографии", description = "Возможные операции с фотографиями")
+@Tag(name = "Фотографии", description = "Возможные операции с фотографиями, могут относиться либо к питомцу, либо к отчету")
 @RequestMapping("/photos")
 public class PhotoController {
     private final PhotoService photoService;
@@ -24,8 +25,7 @@ public class PhotoController {
     @Operation(
             summary = "Добавление фотографий питомца в базу",
             responses = {
-                    @ApiResponse(
-                            responseCode = "201",
+                    @ApiResponse(responseCode = "201",
                             description = "Успешное добавление фотографий"
                     ),
                     @ApiResponse(
@@ -35,9 +35,9 @@ public class PhotoController {
             }
     )
     @PostMapping(value = "for-pet/{petId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> addPhotosForPet(@PathVariable Long petId, @RequestBody MultipartFile[] photos) throws IOException {
+    public void addPhotosForPet(@Parameter(description = "ID питомца") @PathVariable Long petId,
+                                @Parameter(description = "Загружаемые фотографии") @RequestBody MultipartFile[] photos) throws IOException {
         photoService.addPhotosForPet(petId, photos);
-        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @Operation(
@@ -54,53 +54,53 @@ public class PhotoController {
             }
     )
     @PostMapping(value = "for-report/{reportId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> addPhotosForReport(@PathVariable Long reportId, @RequestParam MultipartFile[] photos) throws IOException {
+    public void addPhotosForReport(@Parameter(description = "ID отчета") @PathVariable Long reportId,
+                                   @Parameter(description = "Загружаемые фотографии") @RequestParam MultipartFile[] photos) throws IOException {
         photoService.addPhotosForReport(reportId, photos);
-        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @Operation(
-            summary = "Получение фотографии по ID",
+            summary = "Получение фотографий по ID питомца",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "Фотография найдена"
+                            description = "Фотографии найдены"
                     ),
                     @ApiResponse(
                             responseCode = "400",
-                            description = "Фотография не найдена"
+                            description = "Фотографии не найдены"
                     )
             }
     )
-    @GetMapping("/for-pet/{petId}")
-    public ResponseEntity<?> getPhotosForPet(@PathVariable Long petId,
-                                             HttpServletResponse response) {
-        photoService.getPhotosByPetId(petId, response);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping(value = "/for-pet/{petId}/{photoNumber}")
+    public void getPhotosForPet(@Parameter(description = "ID питомца") @PathVariable Long petId,
+                                @Parameter(description = "порядковый номер фотографии") @PathVariable Long photoNumber,
+                                HttpServletResponse response) throws IOException {
+        photoService.getPhotosByPetId(petId, photoNumber, response);
     }
 
     @Operation(
-            summary = "Получение фотографии по ID",
+            summary = "Получение фотографий по ID приюта",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "Фотография найдена"
+                            description = "Фотографии найдены"
                     ),
                     @ApiResponse(
                             responseCode = "400",
-                            description = "Фотография не найдена"
+                            description = "Фотографии не найдены"
                     )
             }
     )
-    @GetMapping("/for-report/{reportId}")
-    public ResponseEntity<?> getPhotosForReport(@PathVariable Long reportId,
-                                                HttpServletResponse response) {
-        photoService.getPhotosByReportId(reportId, response);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/for-report/{reportId}/{photoNumber}")
+    public void getPhotosForReport(@Parameter(description = "ID отчета") @PathVariable Long reportId,
+                                   @Parameter(description = "порядковый номер фотографии") @PathVariable Long photoNumber,
+                                   HttpServletResponse response) throws IOException {
+        photoService.getPhotosByReportId(reportId, photoNumber, response);
     }
 
     @Operation(
-            summary = "Удаление всех фотографий по Id приюта",
+            summary = "Удаление всех фотографий по ID приюта",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
@@ -108,18 +108,18 @@ public class PhotoController {
                     ),
                     @ApiResponse(
                             responseCode = "400",
-                            description = "Фотография не найдена"
+                            description = "У питомца еще нет добавленных фотографий"
                     )
             }
     )
     @DeleteMapping("/for-pet/{petId}")
-    public ResponseEntity<String> deleteAllPhotosByPetId(@PathVariable Long petId) {
+    public ResponseEntity<String> deleteAllPhotosByPetId(@Parameter(description = "ID питомца") @PathVariable Long petId) {
         photoService.deletePhotosByPetId(petId);
         return new ResponseEntity<>("All photos deleted for petId: " + petId, HttpStatus.OK);
     }
 
     @Operation(
-            summary = "Удаление всех фотографий по Id отчета",
+            summary = "Удаление всех фотографий по ID отчета",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
@@ -127,12 +127,12 @@ public class PhotoController {
                     ),
                     @ApiResponse(
                             responseCode = "400",
-                            description = "У отчета еще нет фотографий"
+                            description = "У отчета еще нет добавленных фотографий"
                     )
             }
     )
     @DeleteMapping("/for-report/{reportId}")
-    public ResponseEntity<String> deleteAllPhotosByReportId(@PathVariable Long reportId) {
+    public ResponseEntity<String> deleteAllPhotosByReportId(@Parameter(description = "ID отчета") @PathVariable Long reportId) {
         photoService.deletePhotosByReportId(reportId);
         return new ResponseEntity<>("All photos deleted for reportId: " + reportId, HttpStatus.OK);
     }
