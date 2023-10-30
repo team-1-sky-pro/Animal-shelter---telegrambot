9c0ebbb40dd70474556b8bf01b02fe34d537a133
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-30 11:45:31 +0300

    31/feat: добавление JPA repository для отношения photos

--- src/main/java/pro/sky/animalsheltertelegrambot/service/PhotoService.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/service/PhotoService.java
@@ -1,15 +1,21 @@
 package pro.sky.animalsheltertelegrambot.service;
 
-import pro.sky.animalsheltertelegrambot.model.Adoption;
-import pro.sky.animalsheltertelegrambot.model.Photo;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface PhotoService {
 
-    Photo addPhoto(Photo photo);
+    void addPhotosForPet(Long petId, MultipartFile[] photos) throws IOException;
 
-    Photo getPhoto(Long id);
+    void addPhotosForReport(Long reportId, MultipartFile[] photos) throws IOException;
 
-    Photo updatePhoto(Long id, Photo photo);
+    void getPhotosByPetId(Long petId, HttpServletResponse response);
 
-    void deletePhoto(Long id);
+    void getPhotosByReportId(Long reportId, HttpServletResponse response);
+
+    void deletePhotosByPetId(Long petId);
+
+    void deletePhotosByReportId(Long reportId);
 }
