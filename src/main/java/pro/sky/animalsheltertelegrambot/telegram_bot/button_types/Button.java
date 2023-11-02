ae8b6f3f71a59caf2505b000fa7e018bf3e7a924
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-11-02 19:48:06 +0300

    39/feat: добавление кнопки VOLUNTEER

--- src/main/java/pro/sky/animalsheltertelegrambot/telegram_bot/button_types/Button.java
+++ src/main/java/pro/sky/animalsheltertelegrambot/telegram_bot/button_types/Button.java
@@ -1,11 +1,15 @@
 package pro.sky.animalsheltertelegrambot.telegram_bot.button_types;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum Button {
+
     REPORT("/report", "Отправить отчет о питомце"),
-    ANOTHER_PET("/another_pet", "Взять еще одного питомца");
+    ANOTHER_PET("/another_pet", "Взять еще одного питомца"),
+    VOLUNTEER("/volunteer", "Помощь волонтера");
 
     private final String command;
     private final String text;
